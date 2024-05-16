5b3827bf5f4aadcd62a987d206f9a20de900cdfa
cesarslv <168201664+cesarslv@users.noreply.github.com>
2024-05-16 12:44:44 -0300

    feat: add command printer menu

---
+++ src/app/app/settings/printer/command/_components/comanda-settings-printer-form.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+
+import { Switch } from "@/components/ui/switch";
+import { toast } from "@/components/ui/use-toast";
+
+const notificationsFormSchema = z.object({
+  whatsapp: z.boolean().default(false).optional(),
+  facebook: z.boolean().default(false).optional(),
+  instagram: z.boolean().default(false).optional(),
+  webmenu: z.boolean().default(false).optional(),
+});
+
+type NotificationsFormValues = z.infer<typeof notificationsFormSchema>;
+
+// This can come from your database or API.
+const defaultValues: Partial<NotificationsFormValues> = {
+  whatsapp: false,
+  facebook: false,
+  instagram: true,
+  webmenu: false,
+};
+
+export function ComandaSettingsPrinterForm() {
+  const form = useForm<NotificationsFormValues>({
+    resolver: zodResolver(notificationsFormSchema),
+    defaultValues,
+  });
+
+  function onSubmit(data: NotificationsFormValues) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-12">
+        <div className="space-y-6">
+          <FormField
+            control={form.control}
+            name="whatsapp"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                <div className="space-y-0.5">
+                  <FormLabel className="text-base">
+                    Adicionar o nome dos grupos de adicionais na impressão
+                  </FormLabel>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <p className="font-semibold text-muted-foreground">
+            Exibir notificações chamativa na comanda quando for:
+          </p>
+          <FormField
+            control={form.control}
+            name="facebook"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                <div className="space-y-0.5">
+                  <FormLabel className="text-base">Pra entrega</FormLabel>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="instagram"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                <div className="space-y-0.5">
+                  <FormLabel className="text-base">Retirada no local</FormLabel>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="webmenu"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                <div className="space-y-0.5">
+                  <FormLabel className="text-base">Consumo no local</FormLabel>
+                </div>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                    disabled
+                    aria-readonly
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button type="submit">Salvar</Button>
+      </form>
+    </Form>
+  );
+}
---
+++ src/app/app/settings/printer/command/page.tsx
@@ -0,0 +1,23 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ComandaSettingsPrinterForm } from "./_components/comanda-settings-printer-form";
+
+export default function Page() {
+  return (
+    <div className="grid gap-6">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">3. Comanda</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ComandaSettingsPrinterForm />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}

7a75948747b0745985698336a9546bbdb7bc0475
cesarslv <168201664+cesarslv@users.noreply.github.com>
2024-05-15 22:27:30 -0300

    fix: deploy

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,9 +11,8 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-redundant-type-constituents": "off",
     "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
@@ -23,7 +22,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": [
-      "warn",
+      "off",
       {
         argsIgnorePattern: "^_",
       },
